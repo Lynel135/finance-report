518773d98ed954ab099fcc02ce9d568ae12114b9
Username0957 <foxvenom135@gmail.com>
2025-11-23 12:39:53 +0700

    im

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { AuthProvider } from "@/lib/auth-context"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
+
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
@@ -10,23 +12,15 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Laporan Keuangan Event",
   description: "Sistem laporan keuangan untuk event",
-  generator: "v0.app",
+  generator: "",
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/ccs.jpg",
+        type: "image/jpeg",
       },
     ],
-    apple: "/apple-icon.png",
+    //apple: "/apple-icon.png",
   },
 }
 
